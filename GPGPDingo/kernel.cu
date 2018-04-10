ddef9cbc53d5cd5b831a8022ce00db150a5b19bf
Stachl <hm-stachl@hm.edu>
2018-04-10 12:46:38 +0200

    Timing

--- GPGPDingo/kernel.cu
+++ GPGPDingo/kernel.cu
@@ -1,13 +1,14 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "helper_functions.h"
 
 #include <stdio.h>
 #include <cstdlib>
 #include <math.h>
 // Matrix is ROW Major in memory!
 
-const int matWidth = 1024;
+const int matWidth = 2048;
 const int size = matWidth * matWidth;
 
 void matMulHost(float* result, const float* a, const float* b, int size)
@@ -63,40 +64,58 @@ int main()
 
 	float *cpuResult = (float*)malloc(sizeof(float) * size);
 	float *hostCudaResult = (float*)malloc(sizeof(float) * size);
+
+	// Create timer
+	StopWatchInterface *t;
+	if (!sdkCreateTimer(&t)) {
+		printf("timercreate failed\n");
+		exit(-1);
+	}
+
+	sdkStartTimer(&t);
 	matMulHost(cpuResult, aCpu, bCpu, matWidth);
+	sdkStopTimer(&t);
 
+	printf("Zeitdauer (CPU): %f\n", sdkGetTimerValue(&t));
 
+	sdkResetTimer(&t);
+
+	dim3 blockDim;
+	blockDim.x = matWidth;
+	blockDim.y = matWidth;
 	float *cudaA, *cudaB, *cudaResult;
 	CUDA_ERROR_CHECK(cudaMalloc(&cudaA, sizeof(float) * size));
 	CUDA_ERROR_CHECK(cudaMalloc(&cudaB, sizeof(float) * size));
-	CUDA_ERROR_CHECK(cudaMalloc(&cudaResult, sizeof(float) * size));
+	CUDA_ERROR_CHECK(cudaMalloc(&cudaResult, sizeof(float) * size));	
 
+	sdkStartTimer(&t);
 	CUDA_ERROR_CHECK(cudaMemcpy(cudaA, aCpu, size * sizeof(float), cudaMemcpyHostToDevice));
 	CUDA_ERROR_CHECK(cudaMemcpy(cudaB, bCpu, size * sizeof(float), cudaMemcpyHostToDevice));
-
-	dim3 blockDim;
-	blockDim.x = 1024;
-	blockDim.y = 1024;
-
-	matMulCuda << <blockDim, 1 >> >(cudaResult, cudaA, cudaB, matWidth);
+	matMulCuda << <blockDim, 1 >> > (cudaResult, cudaA, cudaB, matWidth);
 	cudaDeviceSynchronize();
-
 	cudaMemcpy(hostCudaResult, cudaResult, size * sizeof(float), cudaMemcpyDeviceToHost);
-
+	sdkStopTimer(&t);
+	printf("Zeitdauer (GPU): %f\n", sdkGetTimerValue(&t));
 	cudaFree(cudaA);
 	cudaFree(cudaB);
 	cudaFree(cudaResult);
 
 	for (int i = 0; i < size; ++i)
 	{
-		if (cpuResult[i] != hostCudaResult[i])
-			printf("SHITY");
+		if (cpuResult[i] - hostCudaResult[i] > 0.001f)
+		{
+			printf("Floating point issues detected");
+			break;
+		}
 	}
 
 	free(aCpu);
 	free(bCpu);
 	free(cpuResult);
 	free(hostCudaResult);
+
+	sdkDeleteTimer(&t);
+	getchar();
 	return 0;
 }
 
