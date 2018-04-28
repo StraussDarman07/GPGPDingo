2fe8c366d8a24032ac85cf5eaab62eb058e33e5b
Chris Brammer <c.brammer1@gmail.com>
2018-04-28 17:03:00 +0200

    Aufgabe 5

--- OpenCVReadVideo/OpenCVReadVideo.cpp
+++ OpenCVReadVideo/OpenCVReadVideo.cpp
@@ -10,64 +10,110 @@ using namespace cv;
 using namespace std;
 
 extern void toOneChannel(unsigned char *data, int width, int height, int components);
-extern void toGrayScale(unsigned char *data, int width, int height, int components);
+extern void toGrayScale(unsigned char *output, unsigned char *input, int width, int height, int components);
+extern void sobel(unsigned char *output, unsigned char *input, int width, int height);
 
 void singleChannelCuda(Mat& output)
 {
-	void* deviceMem;
-	int elemSize = output.elemSize();
-	size_t size_in_bytes = elemSize * output.cols * output.rows;
-	cudaMalloc(&deviceMem, size_in_bytes);
+    void* deviceMem;
+    size_t elemSize = output.elemSize();
+    size_t size_in_bytes = elemSize * output.cols * output.rows;
+    cudaMalloc(&deviceMem, size_in_bytes);
 
-	cudaMemcpy(deviceMem, output.data, size_in_bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(deviceMem, output.data, size_in_bytes, cudaMemcpyHostToDevice);
 
-	void *args[] = { &deviceMem , &output.cols, &output.rows, &elemSize };
-	cudaLaunchKernel<void>(&toGrayScale, dim3(output.cols / 16 + 1, output.rows / 16 + 1), dim3(16, 16), args);
+    void *args[] = { &deviceMem , &output.cols, &output.rows, &elemSize };
+    cudaLaunchKernel<void>(&toOneChannel, dim3(output.cols / 16 + 1, output.rows / 16 + 1), dim3(16, 16), args);
 
-	cudaDeviceSynchronize();
-	cudaMemcpy(output.data, deviceMem, size_in_bytes, cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    cudaMemcpy(output.data, deviceMem, size_in_bytes, cudaMemcpyDeviceToHost);
 
-	cudaFree(deviceMem);
+    cudaFree(deviceMem);
+}
+
+void greyScaleCuda(Mat& frame, void* output)
+{
+    size_t elemSize = frame.elemSize();
+    size_t size_in_bytes = elemSize * frame.cols * frame.rows;
+
+    void* input;
+    cudaMalloc(&input, size_in_bytes);
+    cudaMemcpy(input, frame.data, size_in_bytes, cudaMemcpyHostToDevice);
+
+    void *args[] = { &output, &input , &frame.cols, &frame.rows, &elemSize };
+    cudaLaunchKernel<void>(&toGrayScale, dim3(frame.cols / 16 + 1, frame.rows / 16 + 1), dim3(16, 16), args);
+
+    cudaDeviceSynchronize();
+    cudaFree(input);
+}
+
+void sobelCuda(Mat& outputFrame, void* input)
+{
+    size_t size_in_bytes = outputFrame.cols * outputFrame.rows;
+
+    void* output;
+    cudaMalloc(&output, size_in_bytes);
+
+    void *args[] = { &output, &input , &outputFrame.cols, &outputFrame.rows };
+    cudaLaunchKernel<void>(&sobel, dim3(outputFrame.cols / 16 + 1, outputFrame.rows / 16 + 1), dim3(16, 16), args);
+
+    cudaDeviceSynchronize();
+    cudaMemcpy(outputFrame.data, output, size_in_bytes, cudaMemcpyDeviceToHost);
+
+    cudaFree(output);
 }
 
 int main(int, char**)
 {
-	//	VideoCapture cap("Z:/Videos/robotica_1080.mp4"); // open the default camera
-	VideoCapture cap("..\\Videos\\robotica_1080.mp4");
-	//	VideoCapture cap("C:/Users/fischer/Downloads/Bennu4k169Letterbox_h264.avi"); // open the default camera
-	//	VideoCapture cap("D:/Users/fischer/Videos/fireworks.mp4");
-	//	VideoCapture cap("D:/Users/fischer/Videos/Bennu4k169Letterbox_h264.mp4");
-	//	VideoCapture cap("D:/Users/fischer/Videos/Bennu4k169Letterbox_h264.avi");
+    //	VideoCapture cap("Z:/Videos/robotica_1080.mp4"); // open the default camera
+    VideoCapture cap("..\\Videos\\robotica_1080.mp4");
+    //	VideoCapture cap("C:/Users/fischer/Downloads/Bennu4k169Letterbox_h264.avi"); // open the default camera
+    //	VideoCapture cap("D:/Users/fischer/Videos/fireworks.mp4");
+    //	VideoCapture cap("D:/Users/fischer/Videos/Bennu4k169Letterbox_h264.mp4");
+    //	VideoCapture cap("D:/Users/fischer/Videos/Bennu4k169Letterbox_h264.avi");
 
-	if (!cap.isOpened())  // check if we succeeded
-		return -1;
+    if (!cap.isOpened())  // check if we succeeded
+        return -1;
 
-	Mat edges;
-	namedWindow("edges", 1);
-	for (;;)
-	{
-		Mat frame;
-		Mat output;
-		cap >> frame; // get a new frame from camera
+    bool firstFrame = true;
+    void* greyScaleBuffer = nullptr;
+    size_t frameBufferSize = 0;
 
-		if (frame.dims == 0) { // we're done
-			break;
-		}
+    namedWindow("edges", 1);
+    for (;;)
+    {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
 
-		output = frame.clone();
-		singleChannelCuda(output);
+        if (frame.dims == 0) { // we're done
+            break;
+        }
 
-		imshow("edges", output);
-		if (waitKey(1) >= 0) break;
-	}
-	// the camera will be deinitialized automatically in VideoCapture destructor
-	return 0;
+        if (firstFrame)
+        {
+            frameBufferSize = frame.cols * frame.rows;
+            cudaMalloc(&greyScaleBuffer, frameBufferSize);
+            firstFrame = false;
+        }
+
+        greyScaleCuda(frame, greyScaleBuffer);
+
+
+        Mat output(frame.rows, frame.cols, CV_8UC1);
+        sobelCuda(output, greyScaleBuffer);
+        imshow("edges", output);
+        if (waitKey(1) >= 0) break;
+    }
+
+    cudaFree(greyScaleBuffer);
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
 
 
 
-	//cvtColor(frame, edges, COLOR_BGR2GRAY);
-	//GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-	//		Sobel(frame, edges, frame.depth(), 2, 2);
-	//		Canny(edges, edges, 0, 30, 3);
-	//		imshow("edges", edges);
+    //cvtColor(frame, edges, COLOR_BGR2GRAY);
+    //GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+    //		Sobel(frame, edges, frame.depth(), 2, 2);
+    //		Canny(edges, edges, 0, 30, 3);
+    //		imshow("edges", edges);
 }
--- OpenCVReadVideo/kernel.cu
+++ OpenCVReadVideo/kernel.cu
@@ -1,4 +1,5 @@
 #include "cuda_runtime.h"
+#include "math_functions.h"
 #include "device_launch_parameters.h"
 
 
@@ -18,7 +19,7 @@ __global__ void toOneChannel(unsigned char *data, int width, int height, int com
     }
 }
 
-__global__ void toGrayScale(unsigned char *data, int width, int height, int components)
+__global__ void toGrayScale(unsigned char *output, unsigned char *input, int width, int height, int components)
 {
     int column = blockIdx.x * blockDim.x + threadIdx.x;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
@@ -26,16 +27,60 @@ __global__ void toGrayScale(unsigned char *data, int width, int height, int comp
     if (row >= height || column >= width)
         return;
 
-    unsigned char * threadData = data + (components * (column + row * width));
+    int index = column + row * width;
+    unsigned char * threadData = input + components * index;
+    unsigned char * outputData = output + index;
 
-    float partRed = 0.299f;
-    float partGreen = 0.587f;
-    float partBlue = 0.114;
+    const float partRed = 0.299f;
+    const float partGreen = 0.587f;
+    const float partBlue = 0.114;
 
     unsigned char greyScale = partBlue * threadData[0] + partGreen * threadData[1] + partRed * threadData[2];
 
-    threadData[0] = greyScale;
-    threadData[1] = greyScale;
-    threadData[2] = greyScale;
+    outputData[0] = greyScale;
+}
 
+__global__ void sobel(unsigned char *output, unsigned char *input, int width, int height)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (y >= height || x >= width)
+        return;
+
+    // Sobel weights
+    float weightsX[9] = { -1, -2, -1,
+                           0,  0,  0,
+                           1,  2,  1 };
+
+    float weightsY[9] = { -1,  0,  1,
+                          -2,  0,  2,
+                          -1,  0,  1 };
+
+    int offsetY[9] = { -1,  -1,  -1,
+                        0,   0,   0,
+                        1,   1,   1 };
+
+    int offsetX[9] = { -1,   0,   1,
+                       -1,   0,   1,
+                       -1,   0,   1 };
+
+
+    float pointX = 0.f;
+    float pointY = 0.f;
+#pragma unroll
+    for (int i = 0; i < 9; i++)
+    {
+        int index = (x + offsetX[i]) + (y + offsetY[i]) * width;
+
+        unsigned char pixel = *(input + index);
+        pointX += pixel * weightsX[i];
+        pointY += pixel * weightsY[i];
+    }
+
+
+    // Do Sobel here!
+    int index = x + y * width;
+    unsigned char * outputData = output + index;
+    outputData[0] = sqrtf(pointX * pointX + pointY * pointY);
 }
