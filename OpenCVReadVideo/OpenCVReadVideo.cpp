50d35f0b2124848d1517dbc1b03e13c0c3a9199a
Thomas Stachl <stachl.thomas@outlook.de>
2018-05-28 16:09:35 +0200

    Abgabe 7

--- OpenCVReadVideo/OpenCVReadVideo.cpp
+++ OpenCVReadVideo/OpenCVReadVideo.cpp
@@ -5,6 +5,7 @@
 
 #include "opencv2/opencv.hpp"
 #include "cuda_runtime.h"
+#include "helper_functions.h"
 
 using namespace cv;
 using namespace std;
@@ -12,55 +13,55 @@ using namespace std;
 extern void toOneChannel(unsigned char *data, int width, int height, int components);
 extern void toGrayScale(unsigned char *output, unsigned char *input, int width, int height, int components);
 extern void sobel(unsigned char *output, unsigned char *input, int width, int height);
-extern void sobelTex(unsigned char *output, cudaTextureObject_t *input, int width, int height);
+extern void sobelTex(unsigned char *output, cudaTextureObject_t input, int width, int height);
 
 void singleChannelCuda(Mat& output)
 {
-    void* deviceMem;
-    size_t elemSize = output.elemSize();
-    size_t size_in_bytes = elemSize * output.cols * output.rows;
-    cudaMalloc(&deviceMem, size_in_bytes);
+	void* deviceMem;
+	size_t elemSize = output.elemSize();
+	size_t size_in_bytes = elemSize * output.cols * output.rows;
+	cudaMalloc(&deviceMem, size_in_bytes);
 
-    cudaMemcpy(deviceMem, output.data, size_in_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(deviceMem, output.data, size_in_bytes, cudaMemcpyHostToDevice);
 
-    void *args[] = { &deviceMem , &output.cols, &output.rows, &elemSize };
-    cudaLaunchKernel<void>(&toOneChannel, dim3(output.cols / 16 + 1, output.rows / 16 + 1), dim3(16, 16), args);
+	void *args[] = { &deviceMem , &output.cols, &output.rows, &elemSize };
+	cudaLaunchKernel<void>(&toOneChannel, dim3(output.cols / 16 + 1, output.rows / 16 + 1), dim3(16, 16), args);
 
-    cudaDeviceSynchronize();
-    cudaMemcpy(output.data, deviceMem, size_in_bytes, cudaMemcpyDeviceToHost);
+	cudaDeviceSynchronize();
+	cudaMemcpy(output.data, deviceMem, size_in_bytes, cudaMemcpyDeviceToHost);
 
-    cudaFree(deviceMem);
+	cudaFree(deviceMem);
 }
 
 void greyScaleCuda(Mat& frame, void* output)
 {
-    size_t elemSize = frame.elemSize();
-    size_t size_in_bytes = elemSize * frame.cols * frame.rows;
+	size_t elemSize = frame.elemSize();
+	size_t size_in_bytes = elemSize * frame.cols * frame.rows;
 
-    void* input;
-    cudaMalloc(&input, size_in_bytes);
-    cudaMemcpy(input, frame.data, size_in_bytes, cudaMemcpyHostToDevice);
+	void* input;
+	cudaMalloc(&input, size_in_bytes);
+	cudaMemcpy(input, frame.data, size_in_bytes, cudaMemcpyHostToDevice);
 
-    void *args[] = { &output, &input , &frame.cols, &frame.rows, &elemSize };
-    cudaLaunchKernel<void>(&toGrayScale, dim3(frame.cols / 16 + 1, frame.rows / 16 + 1), dim3(16, 16), args);
+	void *args[] = { &output, &input , &frame.cols, &frame.rows, &elemSize };
+	cudaLaunchKernel<void>(&toGrayScale, dim3(frame.cols / 16 + 1, frame.rows / 16 + 1), dim3(16, 16), args);
 
-    cudaDeviceSynchronize();
-    cudaFree(input);
+	cudaDeviceSynchronize();
+	cudaFree(input);
 }
 
 void sobelCuda(Mat& outputFrame, void* input)
 {
-    size_t size_in_bytes = outputFrame.cols * outputFrame.rows;
+	size_t size_in_bytes = outputFrame.cols * outputFrame.rows;
 
-    void* output;
-    cudaMalloc(&output, size_in_bytes);
+	void* output;
+	cudaMalloc(&output, size_in_bytes);
 
-    void *args[] = { &output, &input , &outputFrame.cols, &outputFrame.rows };
-    cudaError_t error = cudaLaunchKernel<void>(&sobel, dim3(outputFrame.cols / 16 + 1, outputFrame.rows / 16 + 1), dim3(16, 16), args);
-    cudaDeviceSynchronize();
-    cudaMemcpy(outputFrame.data, output, size_in_bytes, cudaMemcpyDeviceToHost);
+	void *args[] = { &output, &input , &outputFrame.cols, &outputFrame.rows };
+	cudaError_t error = cudaLaunchKernel<void>(&sobel, dim3(outputFrame.cols / 16 + 1, outputFrame.rows / 16 + 1), dim3(16, 16), args);
+	cudaDeviceSynchronize();
+	cudaMemcpy(outputFrame.data, output, size_in_bytes, cudaMemcpyDeviceToHost);
 
-    cudaFree(output);
+	cudaFree(output);
 }
 
 void sobelCudaTex(Mat& outputFrame, void* input)
@@ -69,10 +70,10 @@ void sobelCudaTex(Mat& outputFrame, void* input)
 
 
 	//we have already a greyscaled picture and I'm not sure about the channels anymore
-	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(8, 8, 8, 0, cudaChannelFormatKindUnsigned);
+	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(8, 0, 0, 0, cudaChannelFormatKindUnsigned);
 	cudaArray* cuArray;
 	cudaMallocArray(&cuArray, &channelDesc, outputFrame.cols, outputFrame.rows);
-	cudaMemcpyToArray(cuArray, 0, 0, &input, size_in_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpyToArray(cuArray, 0, 0, input, size_in_bytes, cudaMemcpyHostToDevice);
 
 	struct cudaResourceDesc resDesc;
 	memset(&resDesc, 0, sizeof(resDesc));
@@ -83,12 +84,12 @@ void sobelCudaTex(Mat& outputFrame, void* input)
 	memset(&texDesc, 0, sizeof(texDesc));
 	texDesc.addressMode[0] = cudaAddressModeWrap;
 	texDesc.addressMode[1] = cudaAddressModeWrap;
-	texDesc.filterMode = cudaFilterModeLinear;
+	texDesc.filterMode = cudaFilterModePoint;// cudaFilterModeLinear;
 	texDesc.readMode = cudaReadModeElementType;
 	texDesc.normalizedCoords = 1;
 
 	cudaTextureObject_t texObj = 0;
-	cudaCreateTextureObject(&texObj, &resDesc, &texDesc, NULL);
+	cudaError_t err = cudaCreateTextureObject(&texObj, &resDesc, &texDesc, NULL);
 
 	void* output;
 	cudaMalloc(&output, size_in_bytes);
@@ -108,55 +109,73 @@ void sobelCudaTex(Mat& outputFrame, void* input)
 
 int main(int, char**)
 {
-    //	VideoCapture cap("Z:/Videos/robotica_1080.mp4"); // open the default camera
-    VideoCapture cap("..\\Videos\\robotica_1080.mp4");
-    //	VideoCapture cap("C:/Users/fischer/Downloads/Bennu4k169Letterbox_h264.avi"); // open the default camera
-    //	VideoCapture cap("D:/Users/fischer/Videos/fireworks.mp4");
-    //	VideoCapture cap("D:/Users/fischer/Videos/Bennu4k169Letterbox_h264.mp4");
-    //	VideoCapture cap("D:/Users/fischer/Videos/Bennu4k169Letterbox_h264.avi");
+	//	VideoCapture cap("Z:/Videos/robotica_1080.mp4"); // open the default camera
+	VideoCapture cap("..\\Videos\\robotica_1080.mp4");
+	//	VideoCapture cap("C:/Users/fischer/Downloads/Bennu4k169Letterbox_h264.avi"); // open the default camera
+	//	VideoCapture cap("D:/Users/fischer/Videos/fireworks.mp4");
+	//	VideoCapture cap("D:/Users/fischer/Videos/Bennu4k169Letterbox_h264.mp4");
+	//	VideoCapture cap("D:/Users/fischer/Videos/Bennu4k169Letterbox_h264.avi");
 
-    if (!cap.isOpened())  // check if we succeeded
-        return -1;
+	if (!cap.isOpened())  // check if we succeeded
+		return -1;
 
-    bool firstFrame = true;
-    void* greyScaleBuffer = nullptr;
-    size_t frameBufferSize = 0;
+	bool firstFrame = true;
+	void* greyScaleBuffer = nullptr;
+	size_t frameBufferSize = 0;
 
-    namedWindow("edges", 1);
-    for (;;)
-    {
-        Mat frame;
-        cap >> frame; // get a new frame from camera
+	StopWatchInterface *t;
+	if (!sdkCreateTimer(&t)) {
+		printf("timercreate failed\n");
+		exit(-1);
+	}
 
-        if (frame.dims == 0) { // we're done
-            break;
-        }
+	float time = 0;
+	int frameCount = 0;
+	namedWindow("edges", 1);
+	for (;;)
+	{
+		Mat frame;
+		cap >> frame; // get a new frame from camera
 
-        if (firstFrame)
-        {
-            frameBufferSize = frame.cols * frame.rows;
-            cudaMalloc(&greyScaleBuffer, frameBufferSize);
-            firstFrame = false;
-        }
+		if (frame.dims == 0) { // we're done
+			break;
+		}
 
-        greyScaleCuda(frame, greyScaleBuffer);
+		if (firstFrame)
+		{
+			frameBufferSize = frame.cols * frame.rows;
+			cudaMalloc(&greyScaleBuffer, frameBufferSize);
+			firstFrame = false;
+		}
 
-
-        Mat output(frame.rows, frame.cols, CV_8UC1);
-        sobelCudaTex(output, greyScaleBuffer);
-        imshow("edges", output);
-        if (waitKey(1) >= 0) break;
-    }
-
-    cudaFree(greyScaleBuffer);
-    // the camera will be deinitialized automatically in VideoCapture destructor
-    return 0;
+		greyScaleCuda(frame, greyScaleBuffer);
 
 
+		Mat output(frame.rows, frame.cols, CV_8UC1);
+		// Create timer
 
-    //cvtColor(frame, edges, COLOR_BGR2GRAY);
-    //GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-    //		Sobel(frame, edges, frame.depth(), 2, 2);
-    //		Canny(edges, edges, 0, 30, 3);
-    //		imshow("edges", edges);
+		sdkStartTimer(&t);
+		sobelCuda(output, greyScaleBuffer);
+		sdkStopTimer(&t);
+
+		time += sdkGetTimerValue(&t);
+		frameCount++;
+		sdkResetTimer(&t);
+		imshow("edges", output);
+		if (waitKey(1) >= 0) break;
+	}
+
+	cudaFree(greyScaleBuffer);
+	printf("Average Time per Frame: %fms", time / frameCount);
+	getchar();
+	// the camera will be deinitialized automatically in VideoCapture destructor
+	return 0;
+
+
+
+	//cvtColor(frame, edges, COLOR_BGR2GRAY);
+	//GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+	//		Sobel(frame, edges, frame.depth(), 2, 2);
+	//		Canny(edges, edges, 0, 30, 3);
+	//		imshow("edges", edges);
 }
--- OpenCVReadVideo/kernel.cu
+++ OpenCVReadVideo/kernel.cu
@@ -5,154 +5,186 @@
 
 __global__ void toOneChannel(unsigned char *data, int width, int height, int components)
 {
-    int column = blockIdx.x * blockDim.x + threadIdx.x;
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int column = blockIdx.x * blockDim.x + threadIdx.x;
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (row >= height || column >= width)
-        return;
+	if (row >= height || column >= width)
+		return;
 
-    unsigned char * threadData = data + (components * (column + row * width));
+	unsigned char * threadData = data + (components * (column + row * width));
 
-    for (int i = 0; i < components - 1; i++)
-    {
-        threadData[i] = 0;
-    }
+	for (int i = 0; i < components - 1; i++)
+	{
+		threadData[i] = 0;
+	}
 }
 
 __global__ void toGrayScale(unsigned char *output, unsigned char *input, int width, int height, int components)
 {
-    int column = blockIdx.x * blockDim.x + threadIdx.x;
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int column = blockIdx.x * blockDim.x + threadIdx.x;
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (row >= height || column >= width)
-        return;
+	if (row >= height || column >= width)
+		return;
 
-    int index = column + row * width;
-    unsigned char * threadData = input + components * index;
-    unsigned char * outputData = output + index;
+	int index = column + row * width;
+	unsigned char * threadData = input + components * index;
+	unsigned char * outputData = output + index;
 
-    const float partRed = 0.299f;
-    const float partGreen = 0.587f;
-    const float partBlue = 0.114;
+	const float partRed = 0.299f;
+	const float partGreen = 0.587f;
+	const float partBlue = 0.114;
 
-    unsigned char greyScale = partBlue * threadData[0] + partGreen * threadData[1] + partRed * threadData[2];
+	unsigned char greyScale = partBlue * threadData[0] + partGreen * threadData[1] + partRed * threadData[2];
 
-    outputData[0] = greyScale;
+	outputData[0] = greyScale;
 }
 
 __global__ void sobel(unsigned char *output, unsigned char *input, int width, int height)
 {
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (y >= height || x >= width)
-        return;
+	if (y >= height || x >= width)
+		return;
 
-    const int BLOCK_SIZE = 16;
+	const int BLOCK_SIZE = 16;
 
-    // Where does our data start
-    int blockStartIndexX = blockIdx.x * blockDim.x - 1;
-    int blockStartIndexY = blockIdx.y * blockDim.y - 1;
+	// Where does our data start
+	int blockStartIndexX = blockIdx.x * blockDim.x - 1;
+	int blockStartIndexY = blockIdx.y * blockDim.y - 1;
 
-    // Clamp to edge
-    if (blockStartIndexX < 0)
-        blockStartIndexX = 0;
+	// Clamp to edge
+	if (blockStartIndexX < 0)
+		blockStartIndexX = 0;
 
-    if (blockStartIndexX >= width)
-        blockStartIndexX = blockDim.x - 1;
+	if (blockStartIndexX >= width)
+		blockStartIndexX = blockDim.x - 1;
 
-    if (blockStartIndexY < 0)
-        blockStartIndexY = 0;
+	if (blockStartIndexY < 0)
+		blockStartIndexY = 0;
 
-    if (blockStartIndexY >= height)
-        blockStartIndexY = blockDim.y - 1;
+	if (blockStartIndexY >= height)
+		blockStartIndexY = blockDim.y - 1;
 
-    // Shared Data
-    __shared__ unsigned char pixels[BLOCK_SIZE + 2][BLOCK_SIZE + 2];
+	// Shared Data
+	__shared__ unsigned char pixels[BLOCK_SIZE + 2][BLOCK_SIZE + 2];
 
-    // Where is our data
-    unsigned char* cacheInput = input + (blockStartIndexX + blockStartIndexY * width);
+	// Where is our data
+	unsigned char* cacheInput = input + (blockStartIndexX + blockStartIndexY * width);
 
-    // Linear index (16x16 -> 0..255)
-    int threadIndex = threadIdx.x + threadIdx.y * blockDim.x;
+	// Linear index (16x16 -> 0..255)
+	int threadIndex = threadIdx.x + threadIdx.y * blockDim.x;
 
-    int maxLoadSizeBytes = (BLOCK_SIZE + 2) * (BLOCK_SIZE + 2); // 18x18 Block -> 324 Bytes
-    int maxIndexBytes = maxLoadSizeBytes / sizeof(short); // 18x18 Block -> Index 162
+	int maxLoadSizeBytes = (BLOCK_SIZE + 2) * (BLOCK_SIZE + 2); // 18x18 Block -> 324 Bytes
+	int maxIndexBytes = maxLoadSizeBytes / sizeof(short); // 18x18 Block -> Index 162
 
-    if (threadIndex < maxIndexBytes)
-    {
-        // Calculate offset
-        int offsetInBytes = threadIndex * sizeof(short);
+	if (threadIndex < maxIndexBytes)
+	{
+		// Calculate offset
+		int offsetInBytes = threadIndex * sizeof(short);
 
-        int block_half = (BLOCK_SIZE + 2) / 2;
-        int byteRow = offsetInBytes / (BLOCK_SIZE + 2);
-        int byteCol = threadIndex % block_half * 2;
+		int block_half = (BLOCK_SIZE + 2) / 2;
+		int byteRow = offsetInBytes / (BLOCK_SIZE + 2);
+		int byteCol = threadIndex % block_half * 2;
 
-        int offset = byteCol + byteRow * width;
-        //int offsetBuffer = byteCol + byteRow * (BLOCK_SIZE + 2);
+		int offset = byteCol + byteRow * width;
+		//int offsetBuffer = byteCol + byteRow * (BLOCK_SIZE + 2);
 
-        // Copy Data
-        unsigned char* toLoad = cacheInput + offset;
-        /**(&pixels[0][0] + offsetBuffer) = *toLoad;
-        *(&pixels[0][0] + offsetBuffer + 1) = *(toLoad + 1);*/
+		// Copy Data
+		unsigned char* toLoad = cacheInput + offset;
+		/**(&pixels[0][0] + offsetBuffer) = *toLoad;
+		*(&pixels[0][0] + offsetBuffer + 1) = *(toLoad + 1);*/
 
-        pixels[byteRow][byteCol] = *toLoad;
-        pixels[byteRow][byteCol + 1] = *(toLoad + 1);
-    }
+		pixels[byteRow][byteCol] = *toLoad;
+		pixels[byteRow][byteCol + 1] = *(toLoad + 1);
+	}
 
-    __syncthreads();
-
-
-    // Sobel weights
-    float weightsX[9] = { -1, -2, -1,
-                           0,  0,  0,
-                           1,  2,  1 };
-
-    float weightsY[9] = { -1,  0,  1,
-                          -2,  0,  2,
-                          -1,  0,  1 };
-
-    int offsetY[9] = { -1,  -1,  -1,
-                        0,   0,   0,
-                        1,   1,   1 };
-
-    int offsetX[9] = { -1,   0,   1,
-                       -1,   0,   1,
-                       -1,   0,   1 };
+	__syncthreads();
 
 
-    float pointX = 0.f;
-    float pointY = 0.f;
+	// Sobel weights
+	float weightsX[9] = { -1, -2, -1,
+						   0,  0,  0,
+						   1,  2,  1 };
+
+	float weightsY[9] = { -1,  0,  1,
+						  -2,  0,  2,
+						  -1,  0,  1 };
+
+	int offsetY[9] = { -1,  -1,  -1,
+						0,   0,   0,
+						1,   1,   1 };
+
+	int offsetX[9] = { -1,   0,   1,
+					   -1,   0,   1,
+					   -1,   0,   1 };
+
+
+	float pointX = 0.f;
+	float pointY = 0.f;
 
 #pragma unroll
-    for (int i = 0; i < 9; i++)
-    {
-        int indexX = threadIdx.x + 1 + offsetX[i];
-        int indexY = threadIdx.y + 1 + offsetY[i];
+	for (int i = 0; i < 9; i++)
+	{
+		int indexX = threadIdx.x + 1 + offsetX[i];
+		int indexY = threadIdx.y + 1 + offsetY[i];
 
-        unsigned char pixel = pixels[indexY][indexX];
-        pointX += pixel * weightsX[i];
-        pointY += pixel * weightsY[i];
-    }
+		unsigned char pixel = pixels[indexY][indexX];
+		pointX += pixel * weightsX[i];
+		pointY += pixel * weightsY[i];
+	}
 
 
-    // Do Sobel here!
-    int index = x + y * width;
-    unsigned char * outputData = output + index;
-    outputData[0] = sqrtf(pointX * pointX + pointY * pointY);
+	// Do Sobel here!
+	int index = x + y * width;
+	unsigned char * outputData = output + index;
+	outputData[0] = sqrtf(pointX * pointX + pointY * pointY);
 }
 
 
-__global__ void sobelTex(unsigned char *output, cudaTextureObject_t *input, int width, int height)
+__global__ void sobelTex(unsigned char *output, cudaTextureObject_t input, int width, int height)
 {
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (y >= height || x >= width)
-        return;
+	if (y >= height || x >= width)
+		return;
 
-	unsigned int result = tex2D<unsigned int>(*input,x,y);
-		//just that it is white
-	unsigned char  test = (unsigned char) (int) result;
-    output[y * width + x] = test;
+
+	float weightsX[9] = { -1, -2, -1,
+						   0,  0,  0,
+						   1,  2,  1 };
+
+	float weightsY[9] = { -1,  0,  1,
+						  -2,  0,  2,
+						  -1,  0,  1 };
+
+	int offsetY[9] = { -1,  -1,  -1,
+						0,   0,   0,
+						1,   1,   1 };
+
+	int offsetX[9] = { -1,   0,   1,
+					   -1,   0,   1,
+					   -1,   0,   1 };
+
+
+	float pointX = 0.f;
+	float pointY = 0.f;
+
+#pragma unroll
+	for (int i = 0; i < 9; i++)
+	{
+		int indexX = x + offsetX[i];
+		int indexY = y + offsetY[i];
+
+		float xf = (float)indexX / (float)width;
+		float yf = (float)indexY / (float)height;
+
+		unsigned char pixel = tex2D<unsigned char>(input, xf, yf);
+		pointX += pixel * weightsX[i];
+		pointY += pixel * weightsY[i];
+	}
+
+	output[y * width + x] = sqrtf(pointX * pointX + pointY * pointY);
 }
